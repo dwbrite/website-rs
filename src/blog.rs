f6cceafdbf6560eca487f345c470ae57310a3922
Devin Brite <devin@dwbrite.com>
2021-01-12 18:19:13 -0500

    impl blog; rustfmt; styles; fix posts

---
+++ src/blog.rs
@@ -0,0 +1,130 @@
+use rocket::http::RawStr;
+use rocket::Route;
+use rocket_contrib::templates::Template;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::sync::Arc;
+use toml::value::Datetime;
+
+pub(crate) mod routes {
+    use crate::blog::*;
+    use rocket::{Rocket, State};
+    use rocket_contrib::templates::Template;
+
+    pub(crate) trait MountBlog: Sized {
+        fn mount_blog(self) -> Self;
+    }
+
+    impl MountBlog for Rocket {
+        fn mount_blog(self) -> Self {
+            self.mount("/", routes![blog, blog_post, blog_tags, blog_tag])
+                .manage(BlogState::new())
+        }
+    }
+
+    #[get("/blog")]
+    fn blog(blogstate: State<BlogState>) -> Template {
+        #[derive(Debug, Serialize, Deserialize, PartialEq)]
+        struct Ctx {
+            title: String,
+            posts: Vec<Arc<BlogPost>>,
+        }
+
+        let c = Ctx {
+            title: "dwbrite.com".to_string(),
+            posts: blogstate.sorted_posts.clone(),
+        };
+
+        Template::render("blog", &c)
+    }
+
+    #[get("/blog/post/<_title>")]
+    fn blog_post(_title: &RawStr) -> &'static str {
+        "o hej?"
+    }
+
+    #[get("/blog/tags")]
+    fn blog_tags() -> &'static str {
+        "tags"
+    }
+
+    #[get("/blog/tags/<_name>")]
+    fn blog_tag(_name: &RawStr) -> &'static str {
+        "lol"
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct RawBlogPost {
+    title: String,
+    date: Datetime,
+    tags: Vec<String>,
+    content: String,
+}
+
+impl RawBlogPost {
+    fn into_blog_post(self) -> BlogPost {
+        BlogPost {
+            title: self.title,
+            date: self.date.to_string(),
+            tags: self.tags,
+            content: self.content,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct BlogPost {
+    title: String,
+    date: String,
+    tags: Vec<String>,
+    content: String,
+}
+
+/// TODO: real docs
+struct BlogState {
+    sorted_posts: Vec<Arc<BlogPost>>, // a list of posts sorted by date
+    title_map: HashMap<String, Arc<BlogPost>>, // a map of posts with title as the key
+    tags: HashMap<String, Vec<Arc<BlogPost>>>, // map of tags to posts
+}
+
+impl BlogState {
+    fn new() -> Self {
+        let mut s = Self {
+            sorted_posts: vec![],
+            title_map: Default::default(),
+            tags: Default::default(),
+        };
+
+        s.load_posts();
+        s.sort_posts();
+        s
+    }
+
+    fn load_posts(&mut self) {
+        // TODO: reset
+        let posts_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/blog/posts"));
+
+        for entry in fs::read_dir(posts_path).unwrap() {
+            let entry_path = entry.unwrap().path();
+            let mut file = File::open(entry_path).unwrap();
+            self.sorted_posts.push(Arc::new(Self::read_post(file)));
+        }
+    }
+
+    fn sort_posts(&mut self) {}
+
+    fn read_post(mut file: File) -> BlogPost {
+        let mut s = String::new();
+        file.read_to_string(&mut s).unwrap();
+        // let mut buf_reader = BufReader::new(file);
+        // TODO: toml won't read from ^--this buffer, so I'm converting to str first.
+
+        let post: RawBlogPost = toml::from_str(s.as_str()).unwrap();
+        post.into_blog_post()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,49 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::config::Environment;
-use rocket::Route;
-use rocket::http::Method::Get;
-use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::{BufReader, Read};
+// use crate::blog::routes::mount_blog;
 use rocket::http::RawStr;
-use rocket_contrib::templates::Template;
+use rocket::Route;
 use rocket_contrib::serve::StaticFiles;
-use rocket::logger::LoggingLevel;
-use toml::value::Datetime;
+use rocket_contrib::templates::Template;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct RawBlogPost {
-    title: String,
-    date: Datetime,
-    tags: Vec<String>,
-    content: String,
-}
-
-impl RawBlogPost {
-    fn into_blog_post(self) -> BlogPost {
-        BlogPost {
-            title: self.title,
-            date: self.date.to_string(),
-            tags: self.tags,
-            content: self.content
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct BlogPost {
-    title: String,
-    date: String,
-    tags: Vec<String>,
-    content: String,
-}
+mod blog;
 
 #[get("/")]
 fn home() -> Template {
     #[derive(Debug, Serialize, Deserialize, PartialEq)]
-    struct ctx {
+    struct Ctx {
         title: String,
     }
-    let c = ctx {
-        title: "dwbrite.com".to_string()
+    let c = Ctx {
+        title: "dwbrite.com".to_string(),
     };
     Template::render("home", c)
 }
 
-#[get("/blog")]
-fn blog() -> Template {
-    #[derive(Debug, Serialize, Deserialize, PartialEq)]
-    struct ctx {
-        title: String,
-        posts: Vec<BlogPost>,
-    }
-    let c = ctx {
-        title: "dwbrite.com".to_string(),
-        posts: vec![a()]
-    };
-
-    Template::render("blog", &c)
-}
-
-#[get("/blog/post/<title>")]
-fn blog_post(title: &RawStr) -> &'static str {
-    "o hej?"
-}
-
-#[get("/blog/tags")]
-fn blog_tags() -> &'static str {
-    "tags"
-}
-
-#[get("/blog/tags/<name>")]
-fn blog_tag(name: &RawStr) -> &'static str {
-    "lol"
-}
-
 fn main() {
-    // let home = Route::new(Get, "/", home);
-    // TODO: replace file routes with microservice, a.ohej.us
-    // let files = Route::new(Get, "/files/<file..>", home);
-    // let blog = Route::new(Get, "/blog", blog);
-    a();
+    use blog::routes::MountBlog;
 
     let mut dev = rocket::config::Config::development();
     dev.address = "0.0.0.0".to_string();
 
-    rocket::custom(dev)
+    let mut rocket = rocket::custom(dev);
+    // mount_blog(&mut rocket);
+
+    rocket
         .mount("/", routes![home])
-        .mount("/", routes![blog, blog_post, blog_tags, blog_tag])
-        .mount("/resources", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/resources")))
-        .mount("/blog/media", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/blog/media")))
+        .mount(
+            "/resources",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/resources")),
+        )
+        .mount(
+            "/blog/media",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/blog/media")),
+        )
+        .mount_blog()
         .attach(Template::fairing())
         .launch();
 }
-
-fn a() -> BlogPost {
-    let mut file = File::open("blog/a.toml").unwrap();
-    let mut s = String::new();
-    file.read_to_string(&mut s).unwrap();
-    // let mut buf_reader = BufReader::new(file);
-    // TODO: toml won't read from ^--this buffer, so I'm converting to str first.
-
-    let mut post: RawBlogPost = toml::from_str(s.as_str()).unwrap();
-    post.into_blog_post()
-}
-
-// fn reload_blogposts() {
-//     /*
-//         let s = r#"
-//     title = "pocket progress"
-//     date = 2020-02-22
-//     content = """
-//     <noscript class="media-noscript">
-//     """
-//     "#;
-//
-//
-//     */
-//
-//     // reference counted blogposts
-//
-//     // hashmap of blogposts using titles
-//     // array of posts based on
-//     // _map of vec of blogposts based on tags
-//
-//     // for each file in dir
-//         // check if it's updated
-//         // add it to the (???) template thing
-// }
